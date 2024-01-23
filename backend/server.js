1b70bd468a268d684fe010d37a2bd67318e7281f
Kateryna <k.borysenko.kyiv@gmail.com>
2024-01-23 13:30:20 +0200

    db connect

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
 import products from './data/products.js';
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
@@ -19,4 +22,7 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.listen(port, () => console.log(`💻 Server running on port ${port}`));
+
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port 💻 ${port}`)
+);
