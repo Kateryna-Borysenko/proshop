e11c358e9ea0bfc55d284da0731cee85388143be
Kateryna <k.borysenko.kyiv@gmail.com>
2024-01-24 10:52:06 +0200

    Error middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
 
@@ -16,6 +17,8 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () =>
   console.log(`Server running in ${process.env.NODE_ENV} mode on port 💻 ${port}`)
