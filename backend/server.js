3415e0ce664826d46bb7dc38766151b6b6894bd9
Kateryna <k.borysenko.kyiv@gmail.com>
2024-01-25 15:35:45 +0200

    JWT http-only cookie

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
@@ -11,8 +12,12 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+
+//Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//Cookie parser middleware
+app.use(cookieParser());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
