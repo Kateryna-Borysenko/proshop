42f5e68702e6f83b67a0444610578da1352b3477
Kateryna <k.borysenko.kyiv@gmail.com>
2024-01-31 15:22:31 +0200

    Create product with sample data

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,9 +4,12 @@ import asyncHandler from '../middleware/asyncHandler.js';
 import {
   getProducts,
   getProductById,
+  createProduct,
 } from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 router.route('/').get(getProducts);
 router.route('/:id').get(getProductById);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
 
 export default router;
